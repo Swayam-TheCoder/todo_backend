8f2657900f50c9e590bac6c2e2b7edb0e484637c
Swayam-The Coder <swayamcode1@gmail.com>
2026-02-26 22:43:29 +0530

    bug1

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`)
