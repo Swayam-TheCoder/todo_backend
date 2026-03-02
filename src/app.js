93324b8dd405c5ceefdeadcdbd1b91d9c348bf4e
Swayam-The Coder <swayamcode1@gmail.com>
2026-03-02 11:14:24 +0530

    cors issue fixed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import todoRoutes from "./routes/todo.routes.js";
 
 const app = express();
 
-app.use(cors({ credentials: true }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://full-stack-todo-list-rhb26p4cv-hello-e803509d.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // Postman / server requests
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
 import connectDB from "./config/db.js";
-import todoRoutes from "./routes/todo.routes.js";
+import app from "./app.js";
 
 dotenv.config();
 connectDB();
 
-const app = express();
-
-app.use(express.json());
-app.use("/api/auth", authRoutes);
-app.use("/api/todos", todoRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
